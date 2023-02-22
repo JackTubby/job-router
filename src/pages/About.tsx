45410a5c5a4b3a9b13ef2120b649f80b7132d08d
JackTubby <jack200034@hotmail.com>
2023-02-22 19:37:10 +0000

    init

---
+++ src/pages/About.tsx
@@ -0,0 +1,25 @@
+export const About = () => {
+  return (
+    <div className="about">
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet sequi
+        quos doloremque similique deserunt incidunt ducimus consequuntur illo
+        nulla nisi maxime modi fuga qui repellendus corporis beatae, perferendis
+        ad provident.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet sequi
+        quos doloremque similique deserunt incidunt ducimus consequuntur illo
+        nulla nisi maxime modi fuga qui repellendus corporis beatae, perferendis
+        ad provident.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet sequi
+        quos doloremque similique deserunt incidunt ducimus consequuntur illo
+        nulla nisi maxime modi fuga qui repellendus corporis beatae, perferendis
+        ad provident.
+      </p>
+    </div>
+  );
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Welcome</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        facere beatae architecto repellendus, doloremque voluptates quod ratione
+        nulla accusamus suscipit dolorem illo. Fuga reprehenderit consectetur
+        deserunt sapiente laborum rerum incidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        facere beatae architecto repellendus, doloremque voluptates quod ratione
+        nulla accusamus suscipit dolorem illo. Fuga reprehenderit consectetur
+        deserunt sapiente laborum rerum incidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        facere beatae architecto repellendus, doloremque voluptates quod ratione
+        nulla accusamus suscipit dolorem illo. Fuga reprehenderit consectetur
+        deserunt sapiente laborum rerum incidunt.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
