20b921a3bbceceb8dd74b537c81c0c1248b5be36
JackTubby <jack200034@hotmail.com>
2023-02-22 20:00:29 +0000

    Create basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+//* Imports from react router
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      {/*
+        //* Wrap entire application with the BrowserRouter component then inside we can set our routes
+        //? We do this because BrowserRouter stores the current location in the browser's built-in history stack.
+        //? <BrowserRouter window> defaults to using the current document's defaultView, but it may also be used to track changes to another window's URL, in an <iframe>, for example.
+      */}
+      <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Job Router</h1>
+          {/* //* We add our Link Elements and use to to link to our path
+              //? The reason we use these links is if we do not a call to the server is made each time adding a NavLink intercepts these requests
+              //? We use NavLink and not Link because NavLink gives us the extra ability of it having a class attribute which turns active when the user goes to that route
+          */}
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+        {/* // Inside our main tags we want all our routes */}
+        <main>
+          {/* //* We create our Routes components so we can place our Route components inside
+              //? Routes are kind of a parent component for multiple different route components
+          */}
+          <Routes>
+            {/* 
+              //* We now create our Route components
+              //? We set up a route component for every route we want
+              //? The Route component takes two props path which is the location for this route
+              //? And element which is what we want to show when the route is searched
+        */}
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
