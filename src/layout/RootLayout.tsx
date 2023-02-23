5ca6318f32b3a0e736a71131b1935b193c4030fa
JackTubby <jack200034@hotmail.com>
2023-02-23 10:27:54 +0000

    Add RootLayout and Outlet to show app.tsx routes

---
+++ src/layout/RootLayout.tsx
@@ -0,0 +1,22 @@
+import { NavLink, Outlet } from "react-router-dom";
+//* This layout component is wrapping the other compnents inside App.tsx
+//* We have to use the oulet component to tell react-router where we want to show our Routes in app.tsx
+const RootLayout = () => {
+  return (
+    <div className="route-layout">
+      <header>
+        <nav>
+          <h1>Job Router</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        {/* //* Outputs the route components from App.tsx */}
+        <Outlet/>
+      </main>
+    </div>
+  );
+};
+
+export default RootLayout;
